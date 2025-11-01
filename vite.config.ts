b813a920a11511cd16af3ff3c7c2099e93971e75
Dilip <dilipbk.dev@gmail.com>
2025-11-01 09:36:53 +0545

    feat: created gh config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "kriwin-professional",
   build: {
     // Enable code splitting
     rollupOptions: {
